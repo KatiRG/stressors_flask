353257657691f006d71e8974f28d9053732d687c
katiRG <cnangini@gmail.com>
2016-10-26 16:04:36 +0200

    rechecks radio button that is automatically cleared

--- stressors.py
+++ stressors.py
@@ -8,13 +8,13 @@ def index():
 @app.route('/', methods = ['GET', 'POST'])
 def formhandler():
 	scenario = request.form['scenario']
-	message = "Showing maps for scenario " + scenario + ":"
-	if scenario == "rcp2.6":
+	message = "Showing maps for scenario " + scenario.replace('_','.') + ":"
+	if scenario == "RCP2_6":
 		map1 = "Logo-compact.jpg"
 		map2 = "Logo-compact.jpg"
 		map3 = "Logo-compact.jpg"
 		map4 = "Logo-compact.jpg"
-	elif scenario == "rcp8.5":
+	elif scenario == "RCP8_5":
 		map1 = "sst_rcp8.5.jpg"
 		map2 = "ph_rcp8.5.jpg"
 		map3 = "O2_rcp8.5.jpg"
